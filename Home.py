4ff22f5302ab4914965ca4255fca67885f1a2b19
aomsinon1 <156872565+aomsinon1@users.noreply.github.com>
2025-08-22 11:14:46 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.page_link("Home.py", label="หน้าแรก", icon="🏠")
 st.page_link("pages/DTree.py", label="การทำนายโรคหัวใจวาย", icon="1️⃣")
 st.page_link("pages/NaiveBaye.py", label="การทำนายข้อมูลด้วยเทคนิค Naive Baye", icon="2️⃣", disabled=False)
 st.page_link("http://www.google.com", label="Google", icon="🌎")
